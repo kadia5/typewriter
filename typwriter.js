319849bc60c84971b2f558424fb1f98cd6d583a5
kadia <kdabraeux@gmail.com>
2021-08-25 04:13:53 +0000

    typwriter effect with a specific string, tbf for universal

--- typwriter.js
+++ typwriter.js
@@ -7,18 +7,61 @@
 //     process.stdout.write(char[i])
 //     i ++ }, 2000) // <= 1s delay to make it noticeable. Can dial it down later.
   
-// )}
-const sentence = "hello there from lighthouse labs";
-
-let timer;
-
-let i = 0;
-function type() {
-  process.stdout.write(sentence[i]);
-  i++;
-  if(i >= sentence.length) 
-    clearInterval(timer);
+// }
+// from lighthouse labs
+const sentence = "hello there";
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(sentence.charAt(0));
+    //   process.stdout.write(sentence.charAt(char[0]));
+  }, 0) // <= 1s delay to make it noticeable. Can dial it down later.
+  setTimeout(() => {
+    process.stdout.write(sentence.charAt(1));
+  }, 50);
+  setTimeout(() => {
+    process.stdout.write(sentence.charAt(2));
+  }, 100);
+  setTimeout(() => {
+    process.stdout.write(sentence.charAt(3));
+  }, 150);
+  setTimeout(() => {
+    process.stdout.write(sentence.charAt(4));
+  }, 200);
+  setTimeout(() => {
+    process.stdout.write(sentence.charAt(5));
+  }, 250);
+  setTimeout(() => {
+    process.stdout.write(sentence.charAt(6));
+  }, 300);
+  setTimeout(() => {
+    process.stdout.write(sentence.charAt(7));
+  }, 350);
+  setTimeout(() => {
+    process.stdout.write(sentence.charAt(8));
+  }, 400);
+  setTimeout(() => {
+    process.stdout.write(sentence.charAt(9));
+  }, 450);
+  setTimeout(() => {
+    process.stdout.write(sentence.charAt(10));
+  }, 500) 
+  setTimeout(() => {
+    process.stdout.write(sentence.charAt(11));
+  }, 550) 
+  
+  break
+  
 }
+
+// let timer;
+
+// let i = 0;
+// function type() {
+//   process.stdout.write(sentence[i]);
+//   i++;
+//   if(i >= sentence.length) 
+//     clearInterval(timer);
+// }
 
 // const time = setTimout(type, 500);
 // const suntence = "hello there from lighthouse labs";
